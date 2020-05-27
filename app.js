5eb9a1482b785f00bc079e3ff46fca37f87216a1
Sonu Raghuwanshi <sonu8823812809@gmail.com>
2020-05-27 12:19:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -330,7 +330,14 @@
             {
                 am_pm = 'PM'
             }
-
+            if(hour>12)
+            {
+                hour=hour%12
+            }
+            if(hour==0)
+            {
+                hour=12
+            }
         }
 
 
